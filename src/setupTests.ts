0d9635339db534459b90cdc6041366fa47d6348a
Simeon de Dios <simeon.dedios@gmail.com>
2019-10-17 22:38:59 -0400

    Removed jsdom to fix CI unit tests.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,5 @@
 // imports
-import "jsdom-global/register";
+// import "jsdom-global/register";
 import {configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import "jest-enzyme";
