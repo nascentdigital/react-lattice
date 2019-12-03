f51400041ee20440466a50a7632add8b43e8feea
Simeon de Dios <simeon.dedios@gmail.com>
2019-12-03 11:24:17 -0500

    Removing external deps to fix React downstream dependency.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import url from 'rollup-plugin-url'
 import svgr from '@svgr/rollup'
@@ -24,7 +23,6 @@ export default {
     }
   ],
   plugins: [
-    external(),
     postcss({
       modules: true
     }),
